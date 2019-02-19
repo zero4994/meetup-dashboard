f285c8c8257ab983db2937f2ae353784eeb9146a
zero4994 <cavc94@gmail.com>
2019-02-19 16:37:44 +0900

    Adds simple api call to meetup

--- server/app.js
+++ server/app.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
+const unirest = require("unirest");
+const axios = require("axios");
 
 const initializeServer = () => {
   const app = express();
   app.use(morgan("combined"));
 
   app.get("/api/meetups", async (req, res) => {
-    res.sendStatus(200);
+    console.log("this is the key", process.env.MEETUP_KEY);
+
+    const { data } = await axios({
+      url: `https://api.meetup.com/2/events?key=${
+        process.env.MEETUP_KEY
+      }&group_urlname=ny-tech&sign=true`,
+      method: "get"
+    });
+    res.json(data);
   });
 
   return app;

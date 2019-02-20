9cd14be9d40724554923bd7ab50162617a9b3de1
Brian Lee <40387476+hIkipooh@users.noreply.github.com>
2019-02-20 09:55:02 +0900

    adding material ui components to card section.

--- src/EventCard.jsx
+++ src/EventCard.jsx
@@ -3,6 +3,20 @@ import { connect } from "react-redux";
 import {} from "./action";
 import { withStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
+import classnames from "classnames";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import red from "@material-ui/core/colors/red";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 const styles = {};
 
@@ -18,12 +32,12 @@ class EventCard extends Component {
 }
 
 EventCard.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
-const mapStateToProps = (state) => ({ test: state.test });
+const mapStateToProps = state => ({ test: state.test });
 
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = dispatch => ({});
 
 export default connect(
   mapStateToProps,
